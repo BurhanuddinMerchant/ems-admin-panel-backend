ad54ffb9c0dcb81a8beac2ca03927d47bf6249f6
BURHANUDDIN <Bmerchant945@gmail.com>
2020-10-26 11:28:54 +0530

    updated comments

--- src/routers/Admin.js
+++ src/routers/Admin.js
@@ -4,16 +4,16 @@ const router = new express.Router();
 //create a basic model and then add the authentication middleware
 // const auth = require("../middleware/authentication")
 
-router.get("/admin/login", () => {
-  //this method should create a brand new event
-});
-router.post("/admin/logout", () => {
-  //this method should create a brand new event
-});
-router.post("/admin", () => {
-  //this method should create a brand new event
-});
-router.delete("/admin", () => {
-  //this method should create a brand new event
-});
+// admin login path
+router.get("/admin/login", () => {});
+
+// admin logout path
+router.post("/admin/logout", () => {});
+
+// admin signup path
+router.post("/admin", () => {});
+
+//delete admin
+router.delete("/admin", () => {});
+
 module.exports = router;
